631918ed6c0c4418085c60257f088c6023b43586
danielcarz <dcz9412@gmail.com>
2023-02-20 16:30:52 -0500

    ABOUT ME: the icons of the about me componet has been added

--- src/components/about_me.jsx
+++ src/components/about_me.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+//IMAGES AND ICONS
+import icon_one from '../assets/emoticons/simbolos-de-reiki.png';
+import icon_two from '../assets/emoticons/reiki.png';
+import icon_three from '../assets/emoticons/simbolos-de-reiki(1).png';
+
 const AboutMe = () => {
 
 
@@ -14,9 +19,9 @@ const AboutMe = () => {
                 <h2>About me</h2>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias quidem quaerat recusandae nisi fugit aliquam tenetur praesentium sed corporis facilis laudantium nulla nobis molestias commodi eligendi, est quisquam aliquid laboriosam!</p>
                 <figure className="about_me_icons">
-                    <img src="" alt="icon" />
-                    <img src="" alt="icon" />
-                    <img src="" alt="icon" />
+                    <img src={icon_one} alt="icon" />
+                    <img src={icon_two} alt="icon" />
+                    <img src={icon_three} alt="icon" />
                 </figure>
             </section>
         </section>
