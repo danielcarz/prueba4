c395d60e8a7add5095d2e0c7a4ce55f0cce97412
danielcarz <dcz9412@gmail.com>
2023-02-20 16:16:25 -0500

    ABOUT ME:  layout of about me

--- src/components/about_me.jsx
+++ src/components/about_me.jsx
@@ -5,7 +5,21 @@ const AboutMe = () => {
 
     return (
 
-        <h1>hello about me</h1>
+        <section>
+            <figure className="about_me_images">
+                <img src="" alt="about me images" />
+            </figure>
+
+            <section className="about_me_info_wrapper">
+                <h2>About me</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias quidem quaerat recusandae nisi fugit aliquam tenetur praesentium sed corporis facilis laudantium nulla nobis molestias commodi eligendi, est quisquam aliquid laboriosam!</p>
+                <figure className="about_me_icons">
+                    <img src="" alt="icon" />
+                    <img src="" alt="icon" />
+                    <img src="" alt="icon" />
+                </figure>
+            </section>
+        </section>
     )
 }
 
