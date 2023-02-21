749ccae91ddb8b3bc1bc78b0fc873e7396c8d501
danielcarz <dcz9412@gmail.com>
2023-02-21 18:15:58 -0500

    SASS: adding the className to the introduction component

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 
+//IMAGES
+import logo from '../assets/images/LOGO ASHIRA 2020.png'
+
 const Introduction = () => {
 
 
     return (
 
-        <h1>hello introduction</h1>
+        <section className="introduction_container">
+            <figure className="logo_wrapper">
+                <img src={logo} alt="logo ashira" />
+            </figure>
+
+            <nav className="navbar">
+                <ul>
+                    <a href=""><li>item1</li></a>
+                    <a href=""><li>item1</li></a>
+                    <a href=""><li>item1</li></a>
+                </ul>
+            </nav>
+
+            <section className="lema_ashira_wrapper">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero debitis expedita obcaecati rem dicta, quidem aspernatur earum voluptatum atque fuga.</p>
+            </section>
+        </section>
     )
 }
 
