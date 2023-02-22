95406b129af7247da5f280e3e59444a2c58aab3f
danielcarz <dcz9412@gmail.com>
2023-02-22 09:02:57 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   
 
   return (
-    <div className="App">
+    <div className="App"> 
       <Introduction></Introduction>
       <AboutMe></AboutMe>
       <Servicies></Servicies>
