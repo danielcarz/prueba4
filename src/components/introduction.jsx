cd676324bab63532c74575f71119ac03667c22c6
danielcarz <dcz9412@gmail.com>
2023-02-22 06:44:53 -0500

    SASSlayout,poisitioning,styling about_me component

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -13,17 +13,19 @@ const Introduction = () => {
                 <img src={logo} alt="logo ashira" />
             </figure>
 
-            <nav className="navbar">
-                <ul>
-                    <a href=""><li>item1</li></a>
-                    <a href=""><li>item1</li></a>
-                    <a href=""><li>item1</li></a>
-                </ul>
-            </nav>
-
             <section className="lema_ashira_wrapper">
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero debitis expedita obcaecati rem dicta, quidem aspernatur earum voluptatum atque fuga.</p>
             </section>
+
+            <nav className="navbar">
+                <ul>
+                    <li><a href="">item1</a></li>
+                    <li><a href="">item1</a></li>
+                    <li><a href="">item1</a></li>
+                </ul>
+            </nav>
+
+           
         </section>
     )
 }
